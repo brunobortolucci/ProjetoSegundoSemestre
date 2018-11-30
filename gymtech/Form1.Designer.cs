0fee00ff918d2f903f45500eeed68cf2e737bcb3
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-11-30 17:42:32 -0200

    Update gymtech.pdb

--- gymtech/Form1.Designer.cs
+++ gymtech/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.lblEsqueciSenha.TabIndex = 11;
             this.lblEsqueciSenha.TabStop = true;
             this.lblEsqueciSenha.Text = "Esqueci minha senha";
+            this.lblEsqueciSenha.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblEsqueciSenha_LinkClicked);
             // 
             // btnLogin
             // 
