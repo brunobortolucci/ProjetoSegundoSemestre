32041804a12047bbc1acf101b0943d2f889ad913
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-12-01 19:50:42 -0200

    Update Form1.cs

--- gymtech/Form1.cs
+++ gymtech/Form1.cs
@@ -103,6 +103,12 @@ namespace gymtech
                 {
                     MessageBox.Show("Funcionou - Recepção");
                     ler_recepcao.Close();
+                    
+                    //pegando id, para identificar usuario logado
+                    NpgsqlCommand pegar_id = new NpgsqlCommand("SELECT id_user FROM usuarios WHERE login = '" + txbUser.Text + "'", conexao.conn);
+                    string id = Convert.ToString(pegar_id.ExecuteScalar());
+                    
+
                     this.Hide();
                     frmrec.Show();
                 }
