44c6cd0c361baa7332ef119c2b2475d2271412c4
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-12-01 19:50:46 -0200

    Update frmRecepcao.Designer.cs

--- gymtech/frmRecepcao.Designer.cs
+++ gymtech/frmRecepcao.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbApresentacao = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbApresentacao
+            // 
+            this.lbApresentacao.AutoSize = true;
+            this.lbApresentacao.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbApresentacao.Location = new System.Drawing.Point(27, 30);
+            this.lbApresentacao.Name = "lbApresentacao";
+            this.lbApresentacao.Size = new System.Drawing.Size(70, 24);
+            this.lbApresentacao.TabIndex = 0;
+            this.lbApresentacao.Text = "Label";
             // 
             // frmRecepcao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lbApresentacao);
             this.Name = "frmRecepcao";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmRecepcao";
+            this.Load += new System.EventHandler(this.frmRecepcao_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbApresentacao;
     }
 }
