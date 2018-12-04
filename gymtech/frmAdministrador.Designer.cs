35c951e3cbca12dcf978f9350145bedd7c2227d2
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-12-04 10:57:07 -0200

    Create frmAdministrador.Designer.cs

---
+++ gymtech/frmAdministrador.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace gymtech
+{
+    partial class frmAdministrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAdministrador));
+            this.lblApresentacao = new System.Windows.Forms.Label();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.txbCPF = new System.Windows.Forms.TextBox();
+            this.txbData = new System.Windows.Forms.TextBox();
+            this.txbIdUser = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblIdUser = new System.Windows.Forms.Label();
+            this.txbLogin = new System.Windows.Forms.TextBox();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lblLoginOk = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblApresentacao
+            // 
+            this.lblApresentacao.AutoSize = true;
+            this.lblApresentacao.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApresentacao.Location = new System.Drawing.Point(12, 45);
+            this.lblApresentacao.Name = "lblApresentacao";
+            this.lblApresentacao.Size = new System.Drawing.Size(60, 22);
+            this.lblApresentacao.TabIndex = 0;
+            this.lblApresentacao.Text = "label";
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesconectar.Location = new System.Drawing.Point(675, 22);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(97, 35);
+            this.btnDesconectar.TabIndex = 17;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // txbNome
+            // 
+            this.txbNome.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNome.Location = new System.Drawing.Point(251, 164);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(279, 30);
+            this.txbNome.TabIndex = 18;
+            // 
+            // txbCPF
+            // 
+            this.txbCPF.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbCPF.Location = new System.Drawing.Point(251, 222);
+            this.txbCPF.Name = "txbCPF";
+            this.txbCPF.Size = new System.Drawing.Size(279, 30);
+            this.txbCPF.TabIndex = 19;
+            // 
+            // txbData
+            // 
+            this.txbData.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbData.Location = new System.Drawing.Point(251, 280);
+            this.txbData.Name = "txbData";
+            this.txbData.Size = new System.Drawing.Size(279, 30);
+            this.txbData.TabIndex = 20;
+            // 
+            // txbIdUser
+            // 
+            this.txbIdUser.Enabled = false;
+            this.txbIdUser.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbIdUser.Location = new System.Drawing.Point(490, 111);
+            this.txbIdUser.Name = "txbIdUser";
+            this.txbIdUser.Size = new System.Drawing.Size(40, 30);
+            this.txbIdUser.TabIndex = 21;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(247, 139);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(50, 22);
+            this.lblNome.TabIndex = 22;
+            this.lblNome.Text = "NOME";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpf.Location = new System.Drawing.Point(247, 197);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(40, 22);
+            this.lblCpf.TabIndex = 23;
+            this.lblCpf.Text = "CPF";
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblData.Location = new System.Drawing.Point(247, 255);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(190, 22);
+            this.lblData.TabIndex = 24;
+            this.lblData.Text = "DATA DE NASCIMENTO";
+            // 
+            // lblIdUser
+            // 
+            this.lblIdUser.AutoSize = true;
+            this.lblIdUser.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdUser.Location = new System.Drawing.Point(500, 86);
+            this.lblIdUser.Name = "lblIdUser";
+            this.lblIdUser.Size = new System.Drawing.Size(30, 22);
+            this.lblIdUser.TabIndex = 25;
+            this.lblIdUser.Text = "ID";
+            // 
+            // txbLogin
+            // 
+            this.txbLogin.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbLogin.Location = new System.Drawing.Point(251, 338);
+            this.txbLogin.Name = "txbLogin";
+            this.txbLogin.Size = new System.Drawing.Size(279, 30);
+            this.txbLogin.TabIndex = 26;
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSenha.Location = new System.Drawing.Point(251, 396);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(279, 30);
+            this.txbSenha.TabIndex = 27;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(247, 313);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(60, 22);
+            this.lblLogin.TabIndex = 28;
+            this.lblLogin.Text = "LOGIN";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(247, 371);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(60, 22);
+            this.lblSenha.TabIndex = 29;
+            this.lblSenha.Text = "SENHA";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Enabled = false;
+            this.btnCadastrar.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(440, 474);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(90, 32);
+            this.btnCadastrar.TabIndex = 30;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblLoginOk);
+            this.panel1.Controls.Add(this.btnVerificar);
+            this.panel1.Location = new System.Drawing.Point(12, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(760, 457);
+            this.panel1.TabIndex = 31;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(523, 268);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(71, 29);
+            this.btnVerificar.TabIndex = 32;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lblLoginOk
+            // 
+            this.lblLoginOk.AutoSize = true;
+            this.lblLoginOk.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginOk.Location = new System.Drawing.Point(600, 276);
+            this.lblLoginOk.Name = "lblLoginOk";
+            this.lblLoginOk.Size = new System.Drawing.Size(67, 13);
+            this.lblLoginOk.TabIndex = 33;
+            this.lblLoginOk.Text = "Disponível";
+            this.lblLoginOk.Visible = false;
+            // 
+            // frmAdministrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txbSenha);
+            this.Controls.Add(this.txbLogin);
+            this.Controls.Add(this.lblIdUser);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txbIdUser);
+            this.Controls.Add(this.txbData);
+            this.Controls.Add(this.txbCPF);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.lblApresentacao);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmAdministrador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GYM TECH - Crie seu Usuário";
+            this.Load += new System.EventHandler(this.frmAdministrador_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApresentacao;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.TextBox txbCPF;
+        private System.Windows.Forms.TextBox txbData;
+        private System.Windows.Forms.TextBox txbIdUser;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblIdUser;
+        private System.Windows.Forms.TextBox txbLogin;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label lblLoginOk;
+    }
+}
