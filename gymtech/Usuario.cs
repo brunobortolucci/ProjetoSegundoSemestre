1ae00151845b38ef70290144286075f727108452
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-12-03 23:24:46 -0200

    Create Usuario.cs

---
+++ gymtech/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gymtech
+{
+    class Usuario
+    {
+        private string _id;
+
+        public void pegarID(string id)
+        {
+            this._id = id;
+        }
+    }
+}
