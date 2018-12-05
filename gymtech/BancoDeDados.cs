e9ccc33a3bee1c3b3a5f4c1c133aebb907f8399f
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-12-05 11:00:24 -0200

    Update BancoDeDados.cs

--- gymtech/BancoDeDados.cs
+++ gymtech/BancoDeDados.cs
@@ -14,14 +14,21 @@ namespace gymtech
         Conexao conexao = new Conexao();
 
 
-        //dados usuario
+        //dados usuario     
 
         private int _id;
         private string _loginUsuario;
         private string _senhaUsuario;
         private string _nomeUsuario;
         private string _cpfUsuario;
+        private string _rg;
         private string _dataUsuario;
+        private string _enderecoRua;
+        private string _enderecoNum;
+        private string _enderecoCom;
+        private string _cep;
+        private string _bairro;
+        private string _celular; 
 
         //variaveis uteis
         private string _stringSelect;
@@ -31,6 +38,7 @@ namespace gymtech
         private string _comparacao;
         private string _maxid;        
         private bool _encontrouLogin;
+        private string _retorno;
 
         public void autLogin(string select, string loginUsuario, string senhaUsuario, Conexao conexao, bool encontrouLogin, int valorPermissao, string controle)
         {
@@ -280,6 +288,102 @@ namespace gymtech
             }
 
             
+        }
+
+        public void cadastrarAluno(string select, string insert, int permissao, string nome, string cpf, string rg, string endrua, string endnum, string endcomp, string cep, string bairro, string data, string celular, string login, string senha, int id, Conexao conexao)
+        {
+            //insert into aluno(nome, cpf, rg, endereco_rua, endereco_num, endereco_comp, cep, bairro, data_nasc, celular, id_user)
+
+            _stringSelect = select;
+            _stringSelect = insert;
+            _nomeUsuario = nome;
+            _cpfUsuario = cpf;
+            _rg = rg;
+            _enderecoRua = endrua;
+            _enderecoNum = endnum;
+            _enderecoCom = endcomp;
+            _cep = cep;
+            _bairro = bairro;
+            _dataUsuario = data;
+            _celular = celular;
+            _loginUsuario = login;
+            _senhaUsuario = senha;
+            _id = id;
+            _valorPermissao = permissao;
+
+            _valorPermissao = 2;
+
+            try
+            {
+                _stringInsert = "INSERT INTO usuarios(login, senha, permissao) VALUES (:login, :senha, :permissao)";
+                NpgsqlCommand inserir_usuario = new NpgsqlCommand(_stringInsert, conexao.conn);
+                inserir_usuario.Parameters.Add(new NpgsqlParameter("login", _loginUsuario));
+                inserir_usuario.Parameters.Add(new NpgsqlParameter("senha", _senhaUsuario));
+                inserir_usuario.Parameters.Add(new NpgsqlParameter("permissao", _valorPermissao));
+                inserir_usuario.ExecuteNonQuery();
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            _stringSelect = "SELECT MAX(id_user) FROM usuarios";
+
+            NpgsqlCommand pegarmaxid = new NpgsqlCommand(_stringSelect, conexao.conn);
+            _id = Convert.ToInt32(pegarmaxid.ExecuteScalar());
+
+            try
+            {
+                _stringInsert = "INSERT INTO aluno(nome, cpf, rg, endereco_rua, endereco_num, endereco_comp, cep, bairro, data_nasc, celular, id_user)" +
+                    " VALUES (:nome, :cpf, :rg, :endereco_rua, :endereco_num, :endereco_comp, :cep, :bairro, :data_nasc, :celular, :id_user)";
+
+                NpgsqlCommand inserir_professor = new NpgsqlCommand(_stringInsert, conexao.conn);
+                inserir_professor.Parameters.Add(new NpgsqlParameter("nome", _nomeUsuario));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("cpf", _cpfUsuario));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("rg", _rg));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("endereco_rua", _enderecoRua));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("endereco_num", _enderecoNum));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("endereco_comp", _enderecoCom));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("cep", _cep));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("bairro", _bairro));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("data_nasc", _dataUsuario));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("celular", _celular));
+                inserir_professor.Parameters.Add(new NpgsqlParameter("id_user", _id));
+                inserir_professor.ExecuteNonQuery();
+
+                System.Windows.Forms.MessageBox.Show("Usuario Cadastrado!");
+
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public void buscarAlunoNome(string select, string nome, string retorno, Conexao conexao)
+        {
+            _stringSelect = select;
+            _nomeUsuario = nome;
+            _retorno = retorno;
+
+            _stringSelect = "SELECT nome FROM aluno WHERE nome = '" + _nomeUsuario + "'";
+
+            NpgsqlCommand buscanome = new NpgsqlCommand(_stringSelect, conexao.conn);
+            _retorno = Convert.ToString(buscanome.ExecuteScalar());
+        }
+
+        public void buscarAlunoCpf(string select, string cpf, string retorno, Conexao conexao)
+        {
+            _stringSelect = select;
+            _cpfUsuario = cpf;
+            _retorno = retorno;
+
+            _stringSelect = "SELECT nome FROM aluno WHERE cpf = '" + _cpfUsuario + "'";
+            NpgsqlCommand buscacpf = new NpgsqlCommand(_stringSelect, conexao.conn);
+            _retorno = Convert.ToString(buscacpf.ExecuteScalar());
         }
     }
 }
