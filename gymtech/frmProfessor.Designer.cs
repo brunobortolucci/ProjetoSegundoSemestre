dac22beb9bf3eea1be553bc4c6a45b141f5c50a9
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-12-05 11:00:46 -0200

    Update frmProfessor.Designer.cs

--- gymtech/frmProfessor.Designer.cs
+++ gymtech/frmProfessor.Designer.cs
@@ -33,16 +33,19 @@
             this.lblApresentacao = new System.Windows.Forms.Label();
             this.btnDesconectar = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnCadastrarAluno = new System.Windows.Forms.Button();
-            this.btnCadastrarTreino = new System.Windows.Forms.Button();
-            this.btnEditarAluno = new System.Windows.Forms.Button();
-            this.btnEditarTreino = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btnBuscar = new System.Windows.Forms.Button();
-            this.lblBusca = new System.Windows.Forms.Label();
-            this.checkNome = new System.Windows.Forms.CheckBox();
             this.checkCpf = new System.Windows.Forms.CheckBox();
+            this.checkNome = new System.Windows.Forms.CheckBox();
+            this.lblBusca = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txbBusca = new System.Windows.Forms.TextBox();
+            this.btnEditarTreino = new System.Windows.Forms.Button();
+            this.btnCadastrarTreino = new System.Windows.Forms.Button();
+            this.btnCadastrarAluno = new System.Windows.Forms.Button();
+            this.linklblRetorno = new System.Windows.Forms.LinkLabel();
+            this.painelBusca = new System.Windows.Forms.Panel();
+            this.lblResultado = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.painelBusca.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblUserLogado
@@ -83,72 +86,37 @@
             this.panel1.Controls.Add(this.checkNome);
             this.panel1.Controls.Add(this.lblBusca);
             this.panel1.Controls.Add(this.btnBuscar);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.txbBusca);
             this.panel1.Controls.Add(this.btnEditarTreino);
-            this.panel1.Controls.Add(this.btnEditarAluno);
             this.panel1.Controls.Add(this.btnCadastrarTreino);
             this.panel1.Controls.Add(this.btnCadastrarAluno);
+            this.panel1.Controls.Add(this.painelBusca);
             this.panel1.Location = new System.Drawing.Point(12, 66);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(760, 457);
             this.panel1.TabIndex = 36;
             // 
-            // btnCadastrarAluno
+            // checkCpf
             // 
-            this.btnCadastrarAluno.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCadastrarAluno.Location = new System.Drawing.Point(41, 101);
-            this.btnCadastrarAluno.Name = "btnCadastrarAluno";
-            this.btnCadastrarAluno.Size = new System.Drawing.Size(174, 96);
-            this.btnCadastrarAluno.TabIndex = 37;
-            this.btnCadastrarAluno.Text = "Cadastrar Aluno";
-            this.btnCadastrarAluno.UseVisualStyleBackColor = true;
+            this.checkCpf.AutoSize = true;
+            this.checkCpf.Location = new System.Drawing.Point(554, 166);
+            this.checkCpf.Name = "checkCpf";
+            this.checkCpf.Size = new System.Drawing.Size(46, 17);
+            this.checkCpf.TabIndex = 43;
+            this.checkCpf.Text = "CPF";
+            this.checkCpf.UseVisualStyleBackColor = true;
+            this.checkCpf.CheckedChanged += new System.EventHandler(this.checkCpf_CheckedChanged);
             // 
-            // btnCadastrarTreino
+            // checkNome
             // 
-            this.btnCadastrarTreino.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCadastrarTreino.Location = new System.Drawing.Point(41, 259);
-            this.btnCadastrarTreino.Name = "btnCadastrarTreino";
-            this.btnCadastrarTreino.Size = new System.Drawing.Size(174, 96);
-            this.btnCadastrarTreino.TabIndex = 38;
-            this.btnCadastrarTreino.Text = "Cadastrar Treino";
-            this.btnCadastrarTreino.UseVisualStyleBackColor = true;
-            // 
-            // btnEditarAluno
-            // 
-            this.btnEditarAluno.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditarAluno.Location = new System.Drawing.Point(261, 101);
-            this.btnEditarAluno.Name = "btnEditarAluno";
-            this.btnEditarAluno.Size = new System.Drawing.Size(174, 96);
-            this.btnEditarAluno.TabIndex = 39;
-            this.btnEditarAluno.Text = "Editar Aluno";
-            this.btnEditarAluno.UseVisualStyleBackColor = true;
-            // 
-            // btnEditarTreino
-            // 
-            this.btnEditarTreino.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditarTreino.Location = new System.Drawing.Point(261, 259);
-            this.btnEditarTreino.Name = "btnEditarTreino";
-            this.btnEditarTreino.Size = new System.Drawing.Size(174, 96);
-            this.btnEditarTreino.TabIndex = 40;
-            this.btnEditarTreino.Text = "Editar Treino";
-            this.btnEditarTreino.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(490, 139);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(244, 20);
-            this.textBox1.TabIndex = 41;
-            // 
-            // btnBuscar
-            // 
-            this.btnBuscar.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.Location = new System.Drawing.Point(662, 166);
-            this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(72, 32);
-            this.btnBuscar.TabIndex = 37;
-            this.btnBuscar.Text = "Buscar";
-            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.checkNome.AutoSize = true;
+            this.checkNome.Location = new System.Drawing.Point(490, 166);
+            this.checkNome.Name = "checkNome";
+            this.checkNome.Size = new System.Drawing.Size(58, 17);
+            this.checkNome.TabIndex = 42;
+            this.checkNome.Text = "NOME";
+            this.checkNome.UseVisualStyleBackColor = true;
+            this.checkNome.CheckedChanged += new System.EventHandler(this.checkNome_CheckedChanged);
             // 
             // lblBusca
             // 
@@ -160,25 +128,85 @@
             this.lblBusca.TabIndex = 37;
             this.lblBusca.Text = "Procurar Aluno";
             // 
-            // checkNome
+            // btnBuscar
             // 
-            this.checkNome.AutoSize = true;
-            this.checkNome.Location = new System.Drawing.Point(490, 166);
-            this.checkNome.Name = "checkNome";
-            this.checkNome.Size = new System.Drawing.Size(58, 17);
-            this.checkNome.TabIndex = 42;
-            this.checkNome.Text = "NOME";
-            this.checkNome.UseVisualStyleBackColor = true;
+            this.btnBuscar.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(662, 166);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(72, 32);
+            this.btnBuscar.TabIndex = 37;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
-            // checkCpf
+            // txbBusca
             // 
-            this.checkCpf.AutoSize = true;
-            this.checkCpf.Location = new System.Drawing.Point(554, 166);
-            this.checkCpf.Name = "checkCpf";
-            this.checkCpf.Size = new System.Drawing.Size(46, 17);
-            this.checkCpf.TabIndex = 43;
-            this.checkCpf.Text = "CPF";
-            this.checkCpf.UseVisualStyleBackColor = true;
+            this.txbBusca.Location = new System.Drawing.Point(490, 139);
+            this.txbBusca.Name = "txbBusca";
+            this.txbBusca.Size = new System.Drawing.Size(244, 20);
+            this.txbBusca.TabIndex = 41;
+            // 
+            // btnEditarTreino
+            // 
+            this.btnEditarTreino.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditarTreino.Location = new System.Drawing.Point(258, 182);
+            this.btnEditarTreino.Name = "btnEditarTreino";
+            this.btnEditarTreino.Size = new System.Drawing.Size(174, 96);
+            this.btnEditarTreino.TabIndex = 40;
+            this.btnEditarTreino.Text = "Editar Treino";
+            this.btnEditarTreino.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrarTreino
+            // 
+            this.btnCadastrarTreino.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrarTreino.Location = new System.Drawing.Point(41, 259);
+            this.btnCadastrarTreino.Name = "btnCadastrarTreino";
+            this.btnCadastrarTreino.Size = new System.Drawing.Size(174, 96);
+            this.btnCadastrarTreino.TabIndex = 38;
+            this.btnCadastrarTreino.Text = "Cadastrar Treino";
+            this.btnCadastrarTreino.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrarAluno
+            // 
+            this.btnCadastrarAluno.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrarAluno.Location = new System.Drawing.Point(41, 101);
+            this.btnCadastrarAluno.Name = "btnCadastrarAluno";
+            this.btnCadastrarAluno.Size = new System.Drawing.Size(174, 96);
+            this.btnCadastrarAluno.TabIndex = 37;
+            this.btnCadastrarAluno.Text = "Cadastrar Aluno";
+            this.btnCadastrarAluno.UseVisualStyleBackColor = true;
+            this.btnCadastrarAluno.Click += new System.EventHandler(this.btnCadastrarAluno_Click);
+            // 
+            // linklblRetorno
+            // 
+            this.linklblRetorno.AutoSize = true;
+            this.linklblRetorno.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linklblRetorno.Location = new System.Drawing.Point(4, 32);
+            this.linklblRetorno.Name = "linklblRetorno";
+            this.linklblRetorno.Size = new System.Drawing.Size(77, 15);
+            this.linklblRetorno.TabIndex = 44;
+            this.linklblRetorno.TabStop = true;
+            this.linklblRetorno.Text = "linkLabel1";
+            // 
+            // painelBusca
+            // 
+            this.painelBusca.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.painelBusca.Controls.Add(this.lblResultado);
+            this.painelBusca.Controls.Add(this.linklblRetorno);
+            this.painelBusca.Location = new System.Drawing.Point(490, 226);
+            this.painelBusca.Name = "painelBusca";
+            this.painelBusca.Size = new System.Drawing.Size(200, 74);
+            this.painelBusca.TabIndex = 45;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(4, 4);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(77, 15);
+            this.lblResultado.TabIndex = 45;
+            this.lblResultado.Text = "Resultado:";
             // 
             // frmProfessor
             // 
@@ -197,6 +225,8 @@
             this.Load += new System.EventHandler(this.frmProfessor_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.painelBusca.ResumeLayout(false);
+            this.painelBusca.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -211,11 +241,13 @@
         private System.Windows.Forms.Button btnCadastrarAluno;
         private System.Windows.Forms.Button btnCadastrarTreino;
         private System.Windows.Forms.Button btnEditarTreino;
-        private System.Windows.Forms.Button btnEditarAluno;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txbBusca;
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Label lblBusca;
         private System.Windows.Forms.CheckBox checkCpf;
         private System.Windows.Forms.CheckBox checkNome;
+        private System.Windows.Forms.LinkLabel linklblRetorno;
+        private System.Windows.Forms.Panel painelBusca;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
