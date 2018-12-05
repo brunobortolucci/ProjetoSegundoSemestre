428b5b0092ddedb49aa9ac55a1668d5fc76e48f8
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-12-05 14:45:56 -0200

    Create frmediAluno.cs

---
+++ gymtech/frmediAluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gymtech
+{
+    public partial class frmediAluno : Form
+    {
+        public frmediAluno()
+        {
+            InitializeComponent();
+        }
+    }
+}
