814019ca5dbb5ef1a178c3dd08f7db77901c2909
brunobortolucci <43273001+brunobortolucci@users.noreply.github.com>
2018-12-06 11:05:18 -0200

    Create frmcriarFicha.Designer.cs

---
+++ gymtech/frmcriarFicha.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace gymtech
+{
+    partial class frmcriarFicha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmcriarFicha));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNomeAluno = new System.Windows.Forms.Label();
+            this.lblSave = new System.Windows.Forms.Label();
+            this.comboPerna = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboOmbro = new System.Windows.Forms.ComboBox();
+            this.comboBiceps = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboCostas = new System.Windows.Forms.ComboBox();
+            this.comboPeito = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbPesoAluno = new System.Windows.Forms.TextBox();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.comboSangue = new System.Windows.Forms.ComboBox();
+            this.lblTipSang = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboTriceps = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txbNomeTreino = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastrar ficha de";
+            this.label1.UseMnemonic = false;
+            // 
+            // lblNomeAluno
+            // 
+            this.lblNomeAluno.AutoSize = true;
+            this.lblNomeAluno.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeAluno.Location = new System.Drawing.Point(189, 19);
+            this.lblNomeAluno.Name = "lblNomeAluno";
+            this.lblNomeAluno.Size = new System.Drawing.Size(54, 19);
+            this.lblNomeAluno.TabIndex = 1;
+            this.lblNomeAluno.Text = "label";
+            this.lblNomeAluno.UseMnemonic = false;
+            // 
+            // lblSave
+            // 
+            this.lblSave.AutoSize = true;
+            this.lblSave.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSave.Location = new System.Drawing.Point(709, 9);
+            this.lblSave.Name = "lblSave";
+            this.lblSave.Size = new System.Drawing.Size(27, 19);
+            this.lblSave.TabIndex = 2;
+            this.lblSave.Text = "id";
+            this.lblSave.UseMnemonic = false;
+            this.lblSave.Visible = false;
+            // 
+            // comboPerna
+            // 
+            this.comboPerna.FormattingEnabled = true;
+            this.comboPerna.Location = new System.Drawing.Point(16, 31);
+            this.comboPerna.Name = "comboPerna";
+            this.comboPerna.Size = new System.Drawing.Size(121, 21);
+            this.comboPerna.TabIndex = 3;
+            this.comboPerna.SelectedIndexChanged += new System.EventHandler(this.comboPerna_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PERNA";
+            this.label2.UseMnemonic = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(183, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "OMBRO";
+            this.label3.UseMnemonic = false;
+            // 
+            // comboOmbro
+            // 
+            this.comboOmbro.FormattingEnabled = true;
+            this.comboOmbro.Location = new System.Drawing.Point(187, 31);
+            this.comboOmbro.Name = "comboOmbro";
+            this.comboOmbro.Size = new System.Drawing.Size(121, 21);
+            this.comboOmbro.TabIndex = 6;
+            this.comboOmbro.SelectedIndexChanged += new System.EventHandler(this.comboOmbro_SelectedIndexChanged);
+            // 
+            // comboBiceps
+            // 
+            this.comboBiceps.FormattingEnabled = true;
+            this.comboBiceps.Location = new System.Drawing.Point(16, 144);
+            this.comboBiceps.Name = "comboBiceps";
+            this.comboBiceps.Size = new System.Drawing.Size(121, 21);
+            this.comboBiceps.TabIndex = 7;
+            this.comboBiceps.SelectedIndexChanged += new System.EventHandler(this.comboBiceps_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "BICEPS";
+            this.label4.UseMnemonic = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(183, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "COSTAS";
+            this.label5.UseMnemonic = false;
+            // 
+            // comboCostas
+            // 
+            this.comboCostas.FormattingEnabled = true;
+            this.comboCostas.Location = new System.Drawing.Point(187, 144);
+            this.comboCostas.Name = "comboCostas";
+            this.comboCostas.Size = new System.Drawing.Size(121, 21);
+            this.comboCostas.TabIndex = 10;
+            this.comboCostas.SelectedIndexChanged += new System.EventHandler(this.comboCostas_SelectedIndexChanged);
+            // 
+            // comboPeito
+            // 
+            this.comboPeito.FormattingEnabled = true;
+            this.comboPeito.Location = new System.Drawing.Point(16, 249);
+            this.comboPeito.Name = "comboPeito";
+            this.comboPeito.Size = new System.Drawing.Size(121, 21);
+            this.comboPeito.TabIndex = 11;
+            this.comboPeito.SelectedIndexChanged += new System.EventHandler(this.comboPeito_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 19);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "PEITO";
+            this.label6.UseMnemonic = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(183, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 19);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "TRICEPS";
+            this.label7.UseMnemonic = false;
+            // 
+            // txbPesoAluno
+            // 
+            this.txbPesoAluno.Location = new System.Drawing.Point(434, 289);
+            this.txbPesoAluno.Name = "txbPesoAluno";
+            this.txbPesoAluno.Size = new System.Drawing.Size(100, 20);
+            this.txbPesoAluno.TabIndex = 15;
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(434, 267);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(45, 19);
+            this.lblPeso.TabIndex = 16;
+            this.lblPeso.Text = "PESO";
+            this.lblPeso.UseMnemonic = false;
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(434, 178);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(100, 20);
+            this.txbAltura.TabIndex = 17;
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(430, 156);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(63, 19);
+            this.lblAltura.TabIndex = 18;
+            this.lblAltura.Text = "ALTURA";
+            this.lblAltura.UseMnemonic = false;
+            // 
+            // comboSangue
+            // 
+            this.comboSangue.FormattingEnabled = true;
+            this.comboSangue.Items.AddRange(new object[] {
+            "O-",
+            "O+",
+            "A-",
+            "A+",
+            "B-",
+            "B+",
+            "AB-",
+            "AB+"});
+            this.comboSangue.Location = new System.Drawing.Point(23, 249);
+            this.comboSangue.Name = "comboSangue";
+            this.comboSangue.Size = new System.Drawing.Size(121, 21);
+            this.comboSangue.TabIndex = 19;
+            // 
+            // lblTipSang
+            // 
+            this.lblTipSang.AutoSize = true;
+            this.lblTipSang.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipSang.Location = new System.Drawing.Point(19, 227);
+            this.lblTipSang.Name = "lblTipSang";
+            this.lblTipSang.Size = new System.Drawing.Size(135, 19);
+            this.lblTipSang.TabIndex = 20;
+            this.lblTipSang.Text = "TIPO SANGUÍNEO";
+            this.lblTipSang.UseMnemonic = false;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(619, 463);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(143, 86);
+            this.btnCadastrar.TabIndex = 21;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.comboTriceps);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.comboPerna);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.comboOmbro);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.comboCostas);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.comboPeito);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.comboBiceps);
+            this.panel1.Location = new System.Drawing.Point(29, 144);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(333, 292);
+            this.panel1.TabIndex = 22;
+            // 
+            // comboTriceps
+            // 
+            this.comboTriceps.FormattingEnabled = true;
+            this.comboTriceps.Location = new System.Drawing.Point(187, 249);
+            this.comboTriceps.Name = "comboTriceps";
+            this.comboTriceps.Size = new System.Drawing.Size(121, 21);
+            this.comboTriceps.TabIndex = 15;
+            this.comboTriceps.SelectedIndexChanged += new System.EventHandler(this.comboTriceps_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.lblTipSang);
+            this.panel2.Controls.Add(this.comboSangue);
+            this.panel2.Location = new System.Drawing.Point(410, 144);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(165, 292);
+            this.panel2.TabIndex = 23;
+            // 
+            // txbNomeTreino
+            // 
+            this.txbNomeTreino.Location = new System.Drawing.Point(29, 105);
+            this.txbNomeTreino.Name = "txbNomeTreino";
+            this.txbNomeTreino.Size = new System.Drawing.Size(333, 20);
+            this.txbNomeTreino.TabIndex = 24;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(25, 83);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(135, 19);
+            this.lblNome.TabIndex = 25;
+            this.lblNome.Text = "NOME DO TREINO";
+            this.lblNome.UseMnemonic = false;
+            // 
+            // frmcriarFicha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txbNomeTreino);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.txbPesoAluno);
+            this.Controls.Add(this.lblSave);
+            this.Controls.Add(this.lblNomeAluno);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmcriarFicha";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GYM TECH - Criar Ficha";
+            this.Load += new System.EventHandler(this.frmcriarFicha_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNomeAluno;
+        private System.Windows.Forms.Label lblSave;
+        private System.Windows.Forms.ComboBox comboPerna;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboOmbro;
+        private System.Windows.Forms.ComboBox comboBiceps;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboCostas;
+        private System.Windows.Forms.ComboBox comboPeito;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbPesoAluno;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.ComboBox comboSangue;
+        private System.Windows.Forms.Label lblTipSang;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboTriceps;
+        private System.Windows.Forms.TextBox txbNomeTreino;
+        private System.Windows.Forms.Label lblNome;
+    }
+}
